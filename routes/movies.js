971c58017814b98b503b49c749a1a96d8e532e2a
Luis de Dios Martín <luisddm@gmail.com>
2016-06-20 07:46:14 +0200

    Add CRUD verbs

--- routes/movies.js
+++ routes/movies.js
@@ -3,6 +3,11 @@ const express = require('express');
 
 const router = express.Router();
 
+router.route('/').get((req, res) => {
+  res.json({ message: 'Welcome to our api!' });
+});
+
+// GET movies
 router.route('/movies').get((req, res) => {
   Movie.find((err, movies) => {
     if (err) {
@@ -13,6 +18,7 @@ router.route('/movies').get((req, res) => {
   });
 });
 
+// POST movies
 router.route('/movies').post((req, res) => {
   var movie = new Movie(req.body);
 
@@ -22,6 +28,52 @@ router.route('/movies').post((req, res) => {
     }
 
     res.send({ message: 'Movie Added' });
+  });
+});
+
+// GET movies/:id
+router.route('/movies/:id').get((req, res) => {
+  Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res.send(err);
+    }
+
+    res.json(movie);
+  });
+});
+
+// PUT movies/:id
+router.route('/movies/:id').put((req, res) => {
+  Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res.send(err);
+    }
+
+    for (let prop in req.body) {
+      movie[prop] = req.body[prop];
+    }
+
+    // save the movie
+    movie.save((err) => {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.json({ message: 'Movie updated!' });
+    });
+  });
+});
+
+// DELETE movies/:id
+router.route('/movies/:id').delete((req, res) => {
+  Movie.remove({
+    _id: req.params.id,
+  }, (err, movie) => {
+    if (err) {
+      return res.send(err);
+    }
+
+    res.json({ message: 'Successfully deleted' });
   });
 });
 
